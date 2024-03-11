2b04547286e2dfb9bd82bddf5c3129c6b88b5aa5
jailennn <jailen_ubuntu@SCCKO5CG51131KD.localdomain>
2024-03-11 13:57:41 -0400

    check for apt

--- install.sh
+++ install.sh
@@ -7,3 +7,11 @@ else
 	echo "Script is not being run as root"
 	exit 1
 fi
+
+# Check if the APT binary exists in the PATH
+if command -v apt >/dev/null 2>&1; then
+	    echo "APT package manager is installed."
+    else
+	        echo "APT package manager is not installed."
+fi
+
